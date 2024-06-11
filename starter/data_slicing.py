2e379a855bf9794323a4bb803ab0084e32c72216
rienvl <mbvanleeuwen2003@yahoo.com>
2024-06-11 08:37:48 +0200

    added function for data_slicing

---
+++ starter/data_slicing.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+import logging
+from .starter.train_model import clean_data
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def data_slicing(df):
+    class_labels = df['salary'].unique()
+
+    for class_label in class_labels:
+        logging.info("descriptive stats for {}:\n{}\n".format(
+        class_label, df.loc[df['salary'] == class_label].describe()))
+
+    logging.info("OK - data_slicing.py finished")
+    return
+
+
+if __name__ == '__main__':
+    # load test data
+    full_input_path = os.path.join('starter', 'data', 'census.csv')
+    df = pd.read_csv(full_input_path)
+    df = clean_data(df)
+    data_slicing(df)
