44a9b3daa789fe9156c623b7b661af116c348b3f
rienvl <mbvanleeuwen2003@yahoo.com>
2024-06-15 23:00:56 +0200

    added script request_heroku_live_app.py to request post to live app

--- request_heroku_live_app.py
+++ request_heroku_live_app.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from main import InputX
+from starter.main import InputX
 
 
 x_dict = {
@@ -22,11 +22,10 @@ x_dict = {
 
 data = {"json_obj": x_dict}
 
-# response = requests.post('/inference')
-# response = requests.post('https://udacity-project-4-app-3bfaebc2749a.herokuapp.com/')
-#response = requests.post('http://127.0.0.1:8000/inference')
-#response = requests.post('http://localhost:8000/inference', data=json.dumps(data))
-response = requests.post('http://0.0.0.0:8000/inference', data=json.dumps(data))
-#response = requests.post('/url/to/query/', auth=('usr', 'pass'), data=json.dumps(data))
+# request against local:
+# response = requests.post('http://0.0.0.0:8000/inference', data=json.dumps(data))
+
+# request against Heroku live application
+response = requests.post('https://udacity-project-4-app-3bfaebc2749a.herokuapp.com/inference', data=json.dumps(data))
 print(response.status_code)
 print(response.json())
